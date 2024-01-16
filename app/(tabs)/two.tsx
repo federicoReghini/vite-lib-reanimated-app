5de63fec4844ea2811cc83654f835ac1199fb88f
Federico Reghini <federicoreghini@Federicos-Air.station>
2024-01-16 12:04:15 +0100

    wip

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { View } from "../../components/Themed";
+import { AnimatedText, toast } from "federico-vite-lib-reanimated";
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Pressable
+        onPress={() =>
+          toast.success({ message: "wow", status: "200", duration: 10000 })
+        }
+      >
+        <AnimatedText />
+      </Pressable>
     </View>
   );
 }
@@ -16,16 +20,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
